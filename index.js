7f99172342138c548807c82d4156559adc980dde
songwoo <thddn191@gmail.com>
2021-02-26 03:42:53 +0900

    ~lesson9 password hashing with Bcrypt

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-const express = require('express')
- // expresss 모듈을 node_modules로 부터 갖고 온다.
-const app = express()
-const port = 5000
+const express = require("express");
+// expresss 모듈을 node_modules로 부터 갖고 온다.
+const app = express();
+const port = 5000;
 
-const config = require('./config/key')
-const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then( ()=> console.log("MongoDB is connected"))
-    .catch(err => console.log(err))
+const config = require("./config/key");
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!!!!'))
- // '/'에 오면 'Hello World!!!!' 출력
+app.get("/", (req, res) => res.send("Hello World!!!!"));
+// '/'에 오면 'Hello World!!!!' 출력
 
 const { User } = require("./models/User.js");
-const bodyParser = require('body-parser');
- // 클라이언트에서 받아온 정보를 서버사이드에서 분석해서 req할 수 있도록 하는 것이 bodyParser의 역할
+const bodyParser = require("body-parser");
+// 클라이언트에서 받아온 정보를 서버사이드에서 분석해서 req할 수 있도록 하는 것이 bodyParser의 역할
 
- // application/x-www-form-urlencoded 데이터를 분석해서 갖고올 수 있게 함
-app.use(bodyParser.urlencoded({extended: true}));
- // application/json 타입으로 된 데이터를 분석해서 갖고올 수 있게 함
+// application/x-www-form-urlencoded 데이터를 분석해서 갖고올 수 있게 함
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 타입으로 된 데이터를 분석해서 갖고올 수 있게 함
 app.use(bodyParser.json());
 
 // 회원가입을 위한 route
-app.post('/register', (req, res) => {
-    // 회원가입 시 필요한 정보들을 client에서 가져오면 (req by User.js)
-    // 그것들을 db에 넣어준다
+app.post("/register", (req, res) => {
+  // 회원가입 시 필요한 정보들을 client에서 가져오면 (req by User.js)
+  // 그것들을 db에 넣어준다
 
-    // req.body에는 { id: "thddn", ... } 에 대한 정보가 json타입으로 담겨 있음
-    // 이러한 것은 bodyParser를 이용하였기에 가능한 것임
-    const user = new User(req.body)
+  // req.body에는 { id: "thddn", ... } 에 대한 정보가 json타입으로 담겨 있음
+  // 이러한 것은 bodyParser를 이용하였기에 가능한 것임
+  const user = new User(req.body);
 
-    // mongoDB 메소드이며, save해주면 정보들이 user모델에 저장됨.
-    user.save((err, doc) => {
-        //실패 시에 false와 err메세지 전송
-        if(err) return res.json({ success: false, err})
-        // status(200) 성공했다는 표시
-        return res.status(200).json({
-            success: true
-        })
-    })
-})
- app.listen(port, () => console.log(`Example app listening on port ${port}`))
+  // mongoDB 메소드이며, save해주면 정보들이 user모델에 저장됨.
+  user.save((err, doc) => {
+    //실패 시에 false와 err메세지 전송
+    if (err) return res.json({ success: false, err: err });
+    // status(200) 성공했다는 표시
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
--- models/User.js
+++ models/User.js
@@ -1,32 +1,67 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        trim: true,     //song woo@na.. -> songwoo 빈칸 없애줌
-        unique: 1
-    },
-    lastname: {
-        type: String,
-        maxlength: 50
-    },
-    role: {   //관리자1vs일반유저0
-        type: Number,
-        default: 0
-    },
-    image: String,
-    token: {     //유효성 관리
-        type: String 
-    },
-    tokenExp: {  //토큰 사용할 수 있는 기간
-        type: Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, //song woo@na.. -> songwoo 빈칸 없애줌
+    unique: 1,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  role: {
+    //관리자1vs일반유저0
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    //유효성 관리
+    type: String,
+  },
+  tokenExp: {
+    //토큰 사용할 수 있는 기간
+    type: Number,
+  },
+});
 
-const User = mongoose.model('User', userSchema);
+// * arrow function explicitly prevent binding this !!
+const bcrypt = require("bcrypt");
+// saltRounds = salt가 몇 글자?
+// salt를 생성한 뒤, 이를 이용해서 비밀번호를 암호화 한다.
+const saltRounds = 10;
+//mongoose에서 갖고온 메소드 pre, 인자로 'save'를 주면, save메소드 실행 전에 실행할 행동에 대해서 기술하면 된다.
+userSchema.pre("save", function (next) {
+  var user = this;
 
-module.exports = {User}  //model을 다른 곳에서 사용할 수 있게 함
+  //모델에서 password가 변경될 때만 암호화 코드 실행
+  if (user.isModified("password")) {
+    //비밀번호를 암호화한다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      //next()함수로 save메소드 실행하도록 보내기
+      if (err) return next(err);
+      // myPlaintextPassword(user.password) : 클라이언트가 순수하게 요청한 비밀번호 값 12345같은 거
+      // hash는 암호화된 비밀번호
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User }; //model을 다른 곳에서 사용할 수 있게 함
