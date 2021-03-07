c3ef1539e43136c1caa3557ae846999628e64456
songwoo <thddn191@gmail.com>
2021-03-07 20:11:08 +0900

    ~lesson5 MainImage, Grid component

---
+++ client/src/Config.js
@@ -0,0 +1,4 @@
+export const USER_SERVER = '/api/users';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+export const API_KEY = '9127e2506fafbde9784bfd6f58275716';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,40 +1,67 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import {withRouter} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { withRouter } from "react-router-dom";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../../Config.js";
+import MainImage from "./Sections/MainImage";
 
-function LandingPage(props) {
-  //Landing페이지 왔을 때 제일 먼저 실행되는 메소드
+import GridCards from "../commons/GridCards";
+import { Row } from "antd";
+
+function LandingPage() {
+  // 많은 정보들을 배열에 넣어야 하므로 []
+  // Movies정보들을 state관리하자
+  const [Movies, setMovies] = useState([]);
+  // 가장 인기있는 로고 state관리 + props로 넘기기
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+  //API정보 갖고 오기
   useEffect(() => {
-    //get 요청을 서버에 보냄. endpoint(도착지) = 'api/hello'
-    axios
-      .get("/api/hello")
-      //서버에서 돌아오는 res를 받아 console창에 출력
-      .then((res) => console.log(res.data));
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+    //영화DB 받아오기
+    fetch(endpoint)
+      .then((res) => res.json())
+      .then((res) => {
+        //state에 저장
+        setMovies(res.results);
+        setMainMovieImage(res.results[0]);
+      });
   }, []);
 
-  const onClickHandler = () => {
-    axios.get("api/users/logout").then((res) => {
-      if (res.data.success) {
-        alert("로그아웃 되었습니다.")
-        props.history.push("/login");
-      } else {
-        alert("로그아웃에 실패하였습니다.");
-      }
-    });
-  };
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <h2>시작 페이지</h2>
-      <br />
-      <button onClick={onClickHandler}>로그아웃</button>
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* backdrop_path는 console.log(res.results) 로 확인 */}
+      {MainMovieImage && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+          title={MainMovieImage.original_title}
+          text={MainMovieImage.overview}
+        />
+      )}
+
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movies by latest</h2>
+        <hr />
+        {/* 이미지 사이 여백 주기 */}
+        <Row gutter={[16, 16]}>
+          {Movies &&
+            Movies.map((movie, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  image={
+                    movie.poster_path
+                      ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                      : null
+                  }
+                  movieId={movie.id}
+                  movieName={movie.original_title}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>Load More</button>
+      </div>
     </div>
   );
 }
